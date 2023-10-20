d77d1cd397ecc8179148b6ae9246769170170eb1
ummuqoshiroh <231063117005@student.unsika.ac.id>
2023-10-20 10:56:07 +0700

    menambahkan tugas no1a

---
+++ no1a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    a = 10;
+    b = 8;
+    cout << "a =" << a << endl;
+    cout << "b =" << b << endl;
+    cout << "a =" << b << endl;
+    cout << "b =" << a << endl;
+
+    return 0;
+}
