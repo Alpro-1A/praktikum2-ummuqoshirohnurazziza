ed5c353515028b5a128650daa3febfb59b275b15
ummuqoshiroh <231063117005@student.unsika.ac.id>
2023-10-20 10:59:45 +0700

    mengumpulkan tugas no 1b

---
+++ no1b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    a=10;
+    b=8;
+    cout << "a=" << a << endl;
+    cout << "b=" << b << endl;
+
+    c=a;
+    a=b;
+    b=c;
+    cout << "a=" << a << endl;
+    cout << "b=" << b << endl;
+
+
+
+
+    return 0;
+}
